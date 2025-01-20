6c1fe708e226bf03de79450ab5d975abc58e9bb3
Mestniy183 <ken-zero@ya.ru>
2025-01-20 08:30:06 +0300

    done 576

--- js/swiperComment.js
+++ js/swiperComment.js
@@ -1,7 +1,20 @@
 const swiper = new Swiper('.swiper', {
     loop: true,
+    spaceBetween: 30,
+    speed: 1000,
+    breakpoints: {
+        320: {
+            slidesPerView: 1
+        },
+        576: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        }
+    },
     navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
+        nextEl: '.swiper-button-custom-next',
+        prevEl: '.swiper-button-custom-prev',
     }
 })
