bd4952772580afaa114ba2197408e9fdd989dfcb
Mestniy183 <ken-zero@ya.ru>
2025-01-28 13:21:34 +0300

    animation

--- js/header.js
+++ js/header.js
@@ -1,6 +1,15 @@
 const header = document.querySelector('.header')
 const headerHeight = header.clientHeight
 
+const headerAnimation = [
+    { transfrom: "translateY(0)" },
+    { transfrom: "translateY(-100%)" }
+]
+
+const headerTiming = {
+    duration: 300,
+    iterations: 1
+}
 
 document.addEventListener('scroll', () => {
     let scroll = window.scrollY
@@ -8,5 +17,6 @@ document.addEventListener('scroll', () => {
         header.classList.add('fixed')
     } else {
         header.classList.remove('fixed')
+        header.animate(headerAnimation, headerTiming)
     }
 })
