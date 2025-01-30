e537f2162dcc2b1157481d11fdbd0f787ca30b04
Mestniy183 <ken-zero@ya.ru>
2025-01-30 10:05:17 +0300

    fix-animation

--- js/header.js
+++ js/header.js
@@ -1,23 +1,48 @@
-const header = document.querySelector('.header')
-const headerNav = document.querySelector('.header__nav')
-const headerAddress = document.querySelector('.header__contacts')
-const headerHeight = header.clientHeight
-console.log(headerHeight);
+document.addEventListener('DOMContentLoaded', () => {
+    const header = document.querySelector('.header')
+    const headerNav = document.querySelector('.header__nav')
+    const headerContacts = document.querySelector('.header__contacts')
+    const headerHeight = header.clientHeight
 
-document.addEventListener('scroll', () => {
-    let scroll = window.scrollY
-    console.log(scroll);
-    if (scroll >= 50) {
-        console.log(1);
-        header.classList.add('fixed')
-        headerAddress.classList.add('show')
-        headerNav.classList.add('show')
-    } else {
-        header.classList.add('hidden');
-        setTimeout(() => {
-            header.classList.remove('hidden', 'fixed');
-            headerNav.classList.remove('show')
-            headerAddress.classList.remove('show')
-        }, 300);
+
+    const tl = gsap.timeline({ paused: true });
+
+    function headerFixed() {
+        tl.clear()
+        tl.fromTo('.fixed', { y: "-100%" }, { y: 0, duration: 0.5, ease: "none" })
+        tl.play()
     }
+
+    function showHeaderContent() {
+        tl.fromTo(headerNav, {x: "-100%", autoAlpha: 0}, {x: 0, duration: 0.8, delay: 0.5, autoAlpha: 1, ease: "power4.out"}, 'contacts')
+        tl.fromTo(headerContacts, {x: "100%", autoAlpha: 0}, {x: 0, duration: 0.8, delay: 0.5, autoAlpha: 1, ease: "power4.out"}, 'contacts')
+        tl.play()
+    }
+
+    showHeaderContent()
+
+    function hiddenHeaderFixed() {
+        tl.clear()
+        tl.fromTo('.fixed', { y: 0 }, { y: "-100%", duration: 0.5, ease: "none" })
+        tl.to('.fixed', {y: 0, duration: 0})
+        tl.play()
+    }
+
+    document.addEventListener('scroll', () => {
+        let scroll = window.scrollY
+        if (scroll >= 10) {
+            if (!header.classList.contains('fixed')) {
+                header.classList.add('fixed')
+                headerFixed()
+            }
+        } else {
+           if(header.classList.contains('fixed')){
+            hiddenHeaderFixed()
+            setTimeout(() => {
+                header.classList.remove('fixed')
+                showHeaderContent()
+            }, 500);
+           }
+        }
+    })
 })
