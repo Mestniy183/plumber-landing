c50b7a4cb19e12f10ae1110fdebd488b3dab11bc
Mestniy183 <ken-zero@ya.ru>
2025-02-20 10:03:29 +0300

    modal-form

--- js/modal.js
+++ js/modal.js
@@ -24,31 +24,88 @@ modal.querySelector('.modal__box').addEventListener('click', (e) => {
 })
 
 modal.addEventListener('click', (e) => {
-    if(e._isClickWithinModal) return;
+    if (e._isClickWithinModal) return;
     closeModal()
     clearForm()
 })
 
 window.addEventListener('keydown', (btn) => {
-    if(btn.key === 'Escape') {
+    if (btn.key === 'Escape') {
         closeModal()
         clearForm()
     }
 })
 
-function checkForm() {
-    
-    
+function getIsValid(input, fieldName) {
+
+    const errorText = input.parentElement.querySelector('.error-message')
+    function createError(message) {
+        const newErrorElement = document.createElement('div');
+        newErrorElement.textContent = message;
+        newErrorElement.classList.add('error-message')
+        input.parentElement.append(newErrorElement)
+        input.classList.add('input-invalid')
+    }
+
+    if (errorText) {
+        errorText.remove()
+    }
+
+    if (input.value.trim() === '') {
+        createError(`${fieldName} обязательно для заполнения`)
+        return false
+    }
+
+    input.classList.remove('input-invalid')
+    return true
 }
 
+function validateForm(inputs) {
+    inputs.forEach((input) => {
+        input.addEventListener('input', (e) => {
+            const target = e.target;
+            if (target) {
+                if (target.name === 'name') {
+                    getIsValid(target, 'Имя');
+                } else if (target.name === 'tel') {
+                    getIsValid(target, 'Телефон')
+                } else if (target.name === 'message') {
+                    getIsValid(target, 'Сообщение')
+                }
+            }
+        })
+    })
+}
 
-function sendForm() {
+const inputs = document.querySelectorAll('.form-data')
+validateForm(inputs)
 
+
+const tel = modal.querySelector('input[name="tel"]')
+const maskOptions = {
+    mask: '+7(000)000-00-00',
+    laze: false
+}
+
+const mask = new IMask(tel, maskOptions)
+
+function checkForm() {
+    const inputs = document.querySelectorAll('.form-data')
+    inputs.forEach(input => {
+        if(!getIsValid(input, input.placeholder)) return;
+    })
+    // const name = modal.querySelector('input[name="name"]')
+    // const tel = modal.querySelector('input[name="tel"]')
+    // const message = modal.querySelector('textarea[name="message"]')
+    // if (!getIsValid(name, name.placeholder) || !getIsValid(tel, tel.placeholder) || !getIsValid(message, message.placeholder)) return;
 }
 
 modal.querySelector('.modal__btn').addEventListener('click', (e) => {
     e.preventDefault()
-    sendForm()
-    closeModal()
-    clearForm()
+    
+    checkForm()
+
+
+    // closeModal()
+    // clearForm()
 })
