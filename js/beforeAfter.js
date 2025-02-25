f38c153f3d75e8a971360061c2231c7b5acafd9e
Mestniy183 <ken-zero@ya.ru>
2025-02-25 08:33:55 +0300

    before after

--- js/beforeAfter.js
+++ js/beforeAfter.js
@@ -1,4 +1,43 @@
 const photo = document.querySelector('.example__photo');
-const before = document.querySelector('.example__before');
+const before = photo.querySelector('.example__before');
 const img = before.querySelector('.example__img');
-const change = document.querySelector('.example__change');
+const change = photo.querySelector('.example__change');
+const body = document.body
+let isActive = false
+
+document.addEventListener('DOMContentLoaded', () =>{
+    let widthPhoto = photo.offsetWidth;
+    img.style.width = `${widthPhoto}px`
+})
+
+function pauseEvents (e){
+    e.stopPropagation();
+    e.preventDefault();
+    return false;
+}
+
+photo.addEventListener('mouseup', () =>{
+    isActive = false;
+})
+
+photo.addEventListener('mousedown', () =>{
+    isActive = true;
+})
+
+photo.addEventListener('mouseleave', () =>{
+    isActive = false;
+})
+
+function beforeAfterSlider (x){
+    let shift = Math.max(0, Math.min(x, photo.offsetWidth));
+    before.style.width = `${shift}px`
+    change.style.left = `${shift}px`
+}
+
+body.addEventListener('mousemove', (e) => {
+    if(!isActive) return;
+    let x = e.pageX;
+    x -= photo.getBoundingClientRect().left;
+    beforeAfterSlider(x);
+    pauseEvents(e);
+})
