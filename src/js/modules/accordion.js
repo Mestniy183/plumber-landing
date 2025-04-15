9324c603cc23ecb6f8dc6f12a7a73ec7306a4fc8
mestniy183 <133644102+Mestniy183@users.noreply.github.com>
2025-04-15 08:14:18 +0000

    accordion

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,7 +1,17 @@
 export function accordion() {
   const accordionBtns = document.querySelectorAll(".accordion__btn");
-  accordionBtns.forEach((item, index) => {
-    accordionBtns[index].addEventListener("click", function () {
+  accordionBtns.forEach((btn) => {
+    btn.addEventListener("click", function () {
+      accordionBtns.forEach((otherBtn) => {
+        if (otherBtn !== this) {
+          const otherParent = otherBtn.parentElement;
+          const otherBox = otherBtn.nextElementSibling;
+          otherParent.classList.remove("active");
+          otherBox.style.maxHeight = null;
+          otherBox.style.marginBottom = "0";
+        }
+      });
+
       const parent = this.parentElement;
       parent.classList.toggle("active");
 
