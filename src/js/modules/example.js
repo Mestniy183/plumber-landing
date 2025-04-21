c12a7d673b69d33adfe50d4985676f24ec09b82d
Mestniy183 <ken-zero@ya.ru>
2025-04-21 12:37:36 +0300

    js

--- src/js/modules/example.js
+++ src/js/modules/example.js
@@ -1,3 +1,9 @@
-export function createExamples(){
-    
+export async function createExamples(){
+try {
+    const response = await fetch("../assets/json/example.js");
+    const {examples} = await response.json();
+    console.log(examples); 
+}catch(error){
+    console.log(error);
+}
 }
