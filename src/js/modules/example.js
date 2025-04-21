e0c3e925bf641c05c8d81a1c63edae340322179d
Mestniy183 <ken-zero@ya.ru>
2025-04-21 12:39:58 +0300

    fix js

--- src/js/modules/example.js
+++ src/js/modules/example.js
@@ -1,6 +1,6 @@
 export async function createExamples(){
 try {
-    const response = await fetch("../assets/json/example.js");
+    const response = await fetch("../assets/json/example.json");
     const {examples} = await response.json();
     console.log(examples); 
 }catch(error){
