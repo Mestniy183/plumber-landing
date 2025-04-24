f17821d1afe86474389a09d6295e55062b664806
Mestniy183 <ken-zero@ya.ru>
2025-04-24 10:56:54 +0300

    fix

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,7 @@ import { headerAnimation } from "./modules/header.js";
 import { burger } from "./modules/burger.js";
 import { modal } from "./modules/modal.js";
 import { accordion } from "./modules/accordion.js";
-// import { beforeAfter } from "./modules/beforeAfter.js";
+import { beforeAfter } from "./modules/beforeAfter.js";
 import { getCurrentYear } from "./modules/getCurrentYear.js";
 import {createExamples} from "./modules/example.js";
 import {createComment} from "./modules/comment.js";
@@ -15,5 +15,5 @@ document.addEventListener("DOMContentLoaded", () => {
   createExamples();
   createComment();
   getCurrentYear();
-  // beforeAfter();
+  beforeAfter();
 });
