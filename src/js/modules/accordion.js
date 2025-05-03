b36f93e56676eff1df2f21d20f9edea3b57ec76b
Mestniy183 <ken-zero@ya.ru>
2025-05-03 14:37:13 +0300

    fix js

--- src/js/modules/accordion.js
+++ src/js/modules/accordion.js
@@ -1,30 +1,32 @@
 export function accordion() {
-  const accordionBtns = document.querySelectorAll(".accordion__btn");
-  accordionBtns.forEach((btn) => {
-    btn.addEventListener("click", function () {
-      accordionBtns.forEach((otherBtn) => {
-        if (otherBtn !== this) {
-          const otherParent = otherBtn.parentElement;
-          const otherBox = otherBtn.nextElementSibling;
-          otherParent.classList.remove("active");
-          otherBox.style.maxHeight = null;
-          otherBox.style.marginBottom = "0";
-        }
-      });
+  const accordionItems = document.querySelectorAll(".accordion__item");
+  const toggleAccordion = (item, forceClose = false) => {
+    const isActive = item.classList.contains('active');
+    const btn = item.querySelector('accordion__btn');
+    const box = btn.nextElementSibling;
+    const isMobile = window.innerWidth < 576;
 
-      const parent = this.parentElement;
-      parent.classList.toggle("active");
+    if (isActive || forceClose) {
+      item.classList.remove('active');
+      box.style.maxHeight = null;
+      box.style.marginBottom = "0";
+      return;
+    }
 
-      const box = this.nextElementSibling;
+    item.classList.add('active');
+    box.style.maxHeight = box.scrollHeight + "px";
+    box.style.paddingLeft = isMobile ? '11px' : "27px";
+    box.style.marginBottom = isMobile ? '20px' : "28px";
+  }
 
-      if (box.style.maxHeight) {
-        box.style.maxHeight = null;
-        box.style.marginBottom = "0";
-      } else {
-        box.style.maxHeight = box.scrollHeight + "px";
-        box.style.paddingLeft = "27px";
-        box.style.marginBottom = "28px";
-      }
-    });
-  });
+ document.addEventListener('click', (e) => {
+  const btn = e.target.closest('.accordion__btn');
+  if(!btn) return;
+  const currentItem = btn.closest('.accordion__item');
+
+  accordionItems.forEach(item =>{
+    if(item !== currentItem) toggleAccordion(item, true);
+  })
+  toggleAccordion(currentItem);
+ });
 }
