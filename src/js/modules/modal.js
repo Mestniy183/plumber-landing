14bf905875a3697117169637934aa539ce5fe9ae
mestniy183 <133644102+Mestniy183@users.noreply.github.com>
2025-05-29 11:03:56 +0000

    fix

--- src/js/modules/modal.js
+++ src/js/modules/modal.js
@@ -6,13 +6,13 @@ require("dotenv").config();
 
 const firebaseConfig = {
   apiKey: process.env.VITE_FIREBASE_API_KEY,
-  authDomain: process.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.meta.env.VITE_FIREBASE_DATABASE_URL,
-  projectId: process.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: process.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: process.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  authDomain: process.env.VITE_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VITE_FIREBASE_DATABASE_URL,
+  projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VITE_FIREBASE_APP_ID,
+  measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
