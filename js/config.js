ebed1f2b2ce38e5f43dae3c8da69878ef37a2e15
Mestniy183 <133644102+Mestniy183@users.noreply.github.com>
2025-05-30 05:58:08 +0000

    Deploying to gh-pages from @ Mestniy183/plumber-landing@476e20e75649314b331cc90f16f4995e1414b45d 🚀

---
+++ js/config.js
@@ -0,0 +1,12 @@
+
+    window.firebaseConfig = {
+      apiKey: "undefined",
+      authDomain: "undefined",
+      databaseURL: "undefined",
+      projectId: "undefined",
+      storageBucket: "undefined",
+      messagingSenderId: "undefined",
+      appId: "undefined",
+      measurementId: "undefined"
+    };
+  
