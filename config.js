eb5f1b8090590646530c3c52dd1bafcb7b8562e5
Unnamed-193 <133913095+Unnamed-193@users.noreply.github.com>
2025-06-23 05:41:51 +0000

    Deploying to gh-pages from @ Mestniy183/plumber-landing@869460a7f29aa41d78f14a9775c7c576273ccb7b 🚀

---
+++ config.js
@@ -0,0 +1,4 @@
+window.env = {
+  SUPABASE_URL: 'https://voygehzdwnkrsowhseyh.supabase.co',
+  SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZveWdlaHpkd25rcnNvd2hzZXloIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAzMjI0ODYsImV4cCI6MjA2NTg5ODQ4Nn0.zf0QL4lGuSv1jT4cLPD2UGBEiv4JgSp0lVoLKC47AGc',
+};
