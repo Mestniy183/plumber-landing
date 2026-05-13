11ff91de1c022f8dea499757c5d469309822bca8
Mestniy183 <ken-zero@ya.ru>
2026-05-13 12:16:05 +0300

    api

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+// import { createClient } from '@supabase/supabase-js'
 
-const supabaseURL = window.env.SUPABASE_URL;
-const supabaseKey = window.env.SUPABASE_KEY;
+// const supabaseURL = window.env.SUPABASE_URL;
+// const supabaseKey = window.env.SUPABASE_KEY;
 
-export const supabaseDB = createClient(supabaseURL, supabaseKey)
+// export const supabaseDB = createClient(supabaseURL, supabaseKey)
